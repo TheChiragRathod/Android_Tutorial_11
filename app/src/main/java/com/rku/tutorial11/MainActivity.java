9c9dcb021298f670540d752fc3d58000c89c1561
TheChiragRathod <crathod116@rku.ac.in>
2020-10-05 18:36:48 +0530

    Added Volly Library Code in MainActivity.java to get Data In URL.

--- app/src/main/java/com/rku/tutorial11/MainActivity.java
+++ app/src/main/java/com/rku/tutorial11/MainActivity.java
@@ -9,8 +9,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -23,14 +32,74 @@ public class MainActivity extends AppCompatActivity
 
     ListView listView;
     JSONArray itemjsonArray;
+    ProgressDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setTitle("Users List");
         listView = findViewById(R.id.listView);
+        dialog = new ProgressDialog(this);
 
+
+        setVollyAPI();
 
     }
+
+    public void setVollyAPI()
+    {
+
+        //Volley Library Code Start Here........
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest=new JsonArrayRequest(Request.Method.GET, MyUtil.USER_URL, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    itemjsonArray=response;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                MyAdapter myAdapter=new MyAdapter(MainActivity.this,itemjsonArray);
+                listView.setAdapter(myAdapter);
+                if(dialog.isShowing())
+                    dialog.dismiss();
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+                    {
+                        try {
+                            MyUtil.userJSONObj = itemjsonArray.getJSONObject(position);
+                            Intent intent = new Intent(MainActivity.this, UserData.class);
+                            startActivity(intent);
+
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Somthing went Wrong...", Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog.setTitle("Fetching Data");
+        dialog.show();
+        requestQueue.add(jsonArrayRequest);
+        //Volley Library Code Finished Here........
+
+    }
+
+
+
+
+
 
 }
